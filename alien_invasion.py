1800e1ec593fb56510b0ab0e46303765a74e62fd
Thysmall <jordan@dbimages.com>
2026-04-15 19:07:18 -0400

    P2 - set up ship to reset when colliding with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,8 @@ class AlienInvasion:
         pygame.mixer.init()
         self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
         self.laser_sound.set_volume(0.7)
+        self.impact_sound = pygame.mixer.Sound(self.settings.impact_sound)
+        self.impact_sound.set_volume(0.7)
         
         #Create ship
         self.ship = Ship(self, Arsenal(self))
@@ -44,8 +46,23 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.alien_fleet.update_fleet()
+            self._check_collisions()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _check_collisions(self):
+        # check collisisons for ship
+        if self.ship.check_collisions(self.alien_fleet.fleet):
+            self._reset_level()
+            # subtract one life if possible
+        
+        
+        
+        
+    def _reset_level(self):
+        self.ship.arsenal.arsenal.empty()
+        self.alien_fleet.fleet.empty()
+        self.alien_fleet.create_fleet()
 
     def _update_screen(self):
         """Draws background and other file draw functions then displays
--- ship.py
+++ ship.py
@@ -23,11 +23,15 @@ class Ship:
         
         #Boundary and location creation
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self._center_ship()
         self.moving_right = False
         self.moving_left = False
-        self.x = int(self.rect.x)
+        
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midbottom = self.boundaries.midbottom
+        self.x = int(self.rect.x)
         
     def update(self):
         """Runs update functions for the ship
@@ -60,3 +64,9 @@ class Ship:
             bool: If the bullet was shot or not. Used for sound effect
         """
         return self.arsenal.fire_bullet()
+    
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
